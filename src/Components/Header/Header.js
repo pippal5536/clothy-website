3f2ee566ccf54b09e3bc6a39845a133d303fbdff
pippal5536 <pippal5536@gmail.com>
2022-04-05 02:44:33 +0600

    pic and font added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return (
        <nav>
-           <div>
-               <Link to='/'>Home</Link>
-               <Link to='/dashboard'>Dashboard</Link>
-               <Link to='/reviews'>UserReviews</Link>
-               <Link to='/blogs'>Blogs</Link>
-               <Link to='/about'>About</Link>
+           <div className='font-angel font-light text-4xl flex justify-center p-2 text-white bg-black '>
+               <Link className='mx-4' to='/'>Home</Link>
+               <Link className='mx-4' to='/dashboard'>Dashboard</Link>
+               <Link className='mx-4' to='/reviews'>UserReviews</Link>
+               <Link className='mx-4'  to='/blogs'>Blogs</Link>
+               <Link className='mx-4' to='/about'>About</Link>
                
            </div>
        </nav>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import homeImage from '../../Assets/images/home.jpg'
 
 const Home = () => {
     return (
-        <div>
+       <div>
+            <div>
+            <img className='h-160 w-160' src={homeImage} alt="" />
             
         </div>
+       </div>
     );
 };
 
