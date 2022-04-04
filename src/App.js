7ad13b5fe74811a03b809eb499cee40814935cb3
pippal5536 <pippal5536@gmail.com>
2022-04-05 00:01:38 +0600

    route path added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Blogs from './Components/Blogs/Blogs';
 import UserReviews from './Components/UserReviews/UserReviews';
@@ -11,7 +12,12 @@ import Dashboard from './Components/Dashboard/Dashboard';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Header/>}/> 
+      <Route path="/" element={<Home/>}/> 
+      <Route path="/blogs" element={<Blogs/>}/> 
+      <Route path="/About" element={<About/>}/> 
+      <Route path="/Dashboard" element={<Dashboard/>}/> 
+      <Route path="/UserReviews" element={<UserReviews/>}/> 
+
     </Routes>
   );
 }
