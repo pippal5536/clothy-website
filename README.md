0ca23233f5a0187c058b1c9cccfa919793651af7
pippal5536 <pippal5536@gmail.com>
2022-04-04 23:58:10 +0600

    routes,route imported

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ warp around <App/> in index.js , nested in <BrowserRouter></BrowserRouter>:
 Done
 <BrowserRouter></BrowserRouter> import command:
 import{BrowserRouter} from "react-router-dom";
+
+
+import { Routes, Route } from 'react-router-dom';
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-// import { Routes, Route } from 'react-router-dom'
-import Header from './Components/Header/Header'
-import About from './Components/About/About'
-import Blogs from './Components/Blogs/Blogs'
-import UserReviews from './Components/UserReviews/UserReviews'
-import Dashboard from './Components/Dashboard/Dashboard'
+import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import UserReviews from './Components/UserReviews/UserReviews';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-       </div>
+    <Routes>
+      <Route path="/" element={<Header/>}/> 
+    </Routes>
   );
 }
 
