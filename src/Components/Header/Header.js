afe5851d5840469009f732c9210510db08f1e80b
pippal5536 <pippal5536@gmail.com>
2022-04-05 18:28:28 +0600

    user reviews nav done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Dashboard from '../Dashboard/Dashboard';
+
 
 
 
--- src/Components/UserReviews/UserReviews.js
+++ src/Components/UserReviews/UserReviews.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import UserReviewDetails from '../Userreviewdetails/UserReviewDetails';
 
 const UserReviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect( () =>{
+        fetch('review.json')
+        .then (res => res.json())
+        .then (data => setReviews(data));
+        
+    },[]);
     return (
-        <div>
-            
+        <div className='font-mono mt-16'>
+            <h1 className='text-center text-4xl'>Our Reviews({reviews.length})</h1>
+        {reviews.map(review => (
+        <UserReviewDetails key={review.id} review={review}/>
+        
+        ))}  
+
         </div>
     );
 };
